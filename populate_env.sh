778c8fc33b3d9deca193c2209d35c866755fa75b
Nikolai Kozak <nikokozak@gmail.com>
2021-11-19 22:12:23 -0600

    add gitignore

--- populate_env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Run this by using the . populate_env.sh notation in other shells
-# source populate_env.sh in zsh
-
-export LECTOR_PSQL_USER="lector_chile"
-export LECTOR_PSQL_PASS="lector_chile"
-export LECTOR_PSQL_DATABASE="lector"
