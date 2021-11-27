12fee2e15e169d39ab34688407c061255796b791
Nikolai Kozak <nikokozak@gmail.com>
2021-11-26 22:29:43 -0600

    restructure scraper

---
+++ scraper/scraper/items.py
@@ -0,0 +1,25 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy.loader import ItemLoader
+
+class NoticiaLoader(ItemLoader):
+
+    default_output_processor = TakeFirst()
+    #TODO add functions to deal with data conversion here
+
+class NoticiaItem(scrapy.Item):
+
+    medio = scrapy.Field()
+    seccion = scrapy.Field()
+    titular = scrapy.Field()
+    bajada = scrapy.Field()
+    autor = scrapy.Field()
+    image_url = scrapy.Field()
+    cuerpo = scrapy.Field()
+    fecha = scrapy.Field()
+
+    
