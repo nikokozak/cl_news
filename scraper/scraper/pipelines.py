6dd2f0a2d7c9d7c8dc4f290daf44a17cf381e6c2
Nikolai Kozak <nikokozak@gmail.com>
2021-11-27 18:33:51 -0600

    defaults pipeline

--- scraper/scraper/pipelines.py
+++ scraper/scraper/pipelines.py
@@ -10,3 +10,17 @@ from itemadapter import ItemAdapter
 class ScraperPipeline:
     def process_item(self, item, spider):
         return item
+
+class DefaultsPipeline:
+    '''
+    Simply sets default values for each item.
+    '''
+    def process_item(self, item, spider):
+        item.setdefault('titular', None),
+        item.setdefault('autor', None),
+        item.setdefault('image_url', None),
+        item.setdefault('bajada', None),
+        item.setdefault('cuerpo', None),
+        item.setdefault('fecha', None)
+
+        return item
