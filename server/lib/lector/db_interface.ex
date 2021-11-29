4904485c459fdf806294cac51bb47ec960da3aa6
Nikolai Kozak <nikokozak@gmail.com>
2021-11-28 19:09:21 -0600

    doc for reducer function

--- server/lib/lector/db_interface.ex
+++ server/lib/lector/db_interface.ex
@@ -5,14 +5,26 @@ defmodule Lector.DBInterface do
   @username "lector_chile"
   @database "lector"
 
+  @doc """
+  Retrieves all entries from the 'noticias' table.
+  Maps column names onto each row value, returning an array of
+  Maps.
+  """
   def all do
     {:ok, pid} = Postgrex.start_link(hostname: @hostname, username: @username, database: @database)
 
     %{columns: columns,
       rows: rows } = Postgrex.query!(pid, "SELECT * FROM noticias", [])
 
-    IO.inspect(columns)
-    IO.inspect(rows)
+    Enum.map(rows, fn row -> row_reducer(row, columns) end)
+  end
+
+  defp row_reducer(row, cols) do
+    row
+    |> Enum.with_index
+    |> Enum.reduce(%{}, fn({el, index}, acc) -> 
+      Map.put(acc, Enum.at(cols, index), el)
+    end)
   end
   
 end
