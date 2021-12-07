68d06cf5c4387dd2e6b5d361b30a18ca346e53c4
Nikolai Kozak <nikokozak@gmail.com>
2021-12-06 18:34:42 -0600

    add python avoid sites arg

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -3,23 +3,24 @@ import argparse
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
-parser = argparse.ArgumentParser()
-process = CrawlerProcess(get_project_settings())
 sites = ['biobio', 'tercera']
+process = CrawlerProcess(get_project_settings())
 
+parser = argparse.ArgumentParser()
 parser.add_argument('--sites', type=str, required=False, nargs='+', default="all")
+parser.add_argument('--avoid', type=str, required=False, nargs='+', default=[])
 parser.add_argument('--article-limit', type=int, required=False, default=1)
 parser.add_argument('--clear-cache', action="store_true", required=False, default=False)
-
 args = parser.parse_args()
 
-if args.sites != "all":
-    for site in args.sites:
-        process.crawl(site, limit=args.article_limit, clear_cache=args.clear_cache)
-else:
-    for site in sites:
-        process.crawl(site, limit=args.article_limit, clear_cache=args.clear_cache)
+sites = args.sites if args.sites != "all" else sites
 
-#process.crawl('biobio', limit=1, clear_cache=True)
-#process.crawl('tercera', limit=1, clear_cache=True)
+# Remove from our site list sites we want to avoid
+for avoid in args.avoid:
+    sites.remove(avoid)
+
+for site in sites:
+    #print(site, args.article_limit, args.clear_cache)
+    process.crawl(site, limit=args.article_limit, clear_cache=args.clear_cache)
+
 process.start()
