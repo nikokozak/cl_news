8d4c644f5ec5e567298f24b8d599f034944e3451
Nikolai Kozak <nikokozak@gmail.com>
2021-12-19 09:15:04 -0600

    add crontab creation to scraper deploy

--- scripts/vps_management/deploy_scraper.sh
+++ scripts/vps_management/deploy_scraper.sh
@@ -8,6 +8,7 @@ scp -i "$SSH_KEY" -r ../../scraper ${SSH_USER}@${SSH_SERVER}:/home/${SSH_USER}/s
 ssh_and_run <<-STDIN
     cd srv/scraper
     poetry install
+    sudo echo "*/15 * * * * ${SSH_USER} bash /home/${SSH_USER}/srv/scraper/run_scraper.sh" > /etc/cron.d/scraper
 STDIN
 
 if [ "$?" -eq "0" ]; then
