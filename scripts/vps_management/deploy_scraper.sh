4df3a2efba54fcfc958ed57a2af474cc17558b11
Nikolai Kozak <nikokozak@gmail.com>
2021-12-19 09:18:47 -0600

    remove logs after uploading

--- scripts/vps_management/deploy_scraper.sh
+++ scripts/vps_management/deploy_scraper.sh
@@ -3,12 +3,11 @@
 source ../vps_setup/step0_envs.sh
 source ../functions/ssh_and_run.sh
 
-rm -f ../../scraper/scraper/logs/log.txt
-
 scp -i "$SSH_KEY" -r ../../scraper ${SSH_USER}@${SSH_SERVER}:/home/${SSH_USER}/srv/
 
 ssh_and_run <<-STDIN
     cd srv/scraper
+    rm scraper/logs/log.txt
     poetry install
     sudo echo "*/15 * * * * ${SSH_USER} bash /home/${SSH_USER}/srv/scraper/run_scraper.sh" > /etc/cron.d/scraper
 STDIN
