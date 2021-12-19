992a5629b4ea04328f9cde74517b7358d100c625
Nikolai Kozak <nikokozak@gmail.com>
2021-12-19 09:24:18 -0600

    fix command not found error

--- scripts/vps_management/deploy_scraper.sh
+++ scripts/vps_management/deploy_scraper.sh
@@ -8,7 +8,7 @@ scp -i "$SSH_KEY" -r ../../scraper ${SSH_USER}@${SSH_SERVER}:/home/${SSH_USER}/s
 ssh_and_run <<-STDIN
     cd srv/scraper
     rm scraper/logs/log.txt
-    poetry install
+    ~/.poetry/bin/poetry install
     sudo echo "*/15 * * * * ${SSH_USER} bash /home/${SSH_USER}/srv/scraper/run_scraper.sh" > /etc/cron.d/scraper
 STDIN
 
