75e8f902e8fe983a944040adae43bdc9928bd9b4
Nikolai Kozak <nikokozak@gmail.com>
2021-12-20 20:51:28 -0600

    random port for CD of server, server deploy writes new Caddyfile

--- scripts/vps_management/deploy_server.sh
+++ scripts/vps_management/deploy_server.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# Generate a random num from 8000 - 9000
+LECTOR_PORT=$((8000 + $RANDOM % 1000))
+
 source ../vps_setup/step0_envs.sh
 source ../functions/ssh_and_run.sh
 
@@ -17,6 +20,12 @@ RELEASE_FILENAME=$(basename "$RELEASE_FILE")
 
 scp -i "$SSH_KEY" release/release.tar.gz ${SSH_USER}@${SSH_SERVER}:/home/${SSH_USER}/
 
+cat <<-EOF > ../caddy.conf/Caddyfile
+    lectorchile.com
+
+    reverse_proxy 127.0.0.1:${LECTOR_PORT}
+EOF
+
 ssh_and_run <<-STDIN
     mkdir -p srv/server/ 
     rm -rf srv/server/*
@@ -25,8 +34,12 @@ ssh_and_run <<-STDIN
     cd srv/server/
     sudo tar -zxvf release.tar.gz
 
-    sudo chown lector /home/lector/srv/server
-    sudo chcon -R -t bin_t /home/lector/srv/server/bin/
+    export LECTOR_PORT=${LECTOR_PORT}
+
+    sudo chown $SSH_USER /home/${SSH_USER}/srv/server
+    sudo chcon -R -t bin_t /home/${SSH_USER}/srv/server/bin/
+
+    sudo systemctl reload caddy
 
     systemctl stop lector
     systemctl start lector
