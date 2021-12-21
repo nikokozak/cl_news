53225c5880ca2ba01107c4ea9575325acf420c8f
Nikolai Kozak <nikokozak@gmail.com>
2021-12-21 10:59:24 -0600

    systemd --user space config for server deploy, env variable passing

--- scripts/vps_management/deploy_scraper.sh
+++ scripts/vps_management/deploy_scraper.sh
@@ -6,12 +6,18 @@ source ../functions/ssh_and_run.sh
 scp -i "$SSH_KEY" -r ../../scraper ${SSH_USER}@${SSH_SERVER}:/home/${SSH_USER}/srv/
 
 ssh_and_run <<-STDIN
+
     cd srv/scraper
     rm scraper/logs/log.txt
+
+    # Install deps
     ~/.poetry/bin/poetry install
+
+    # Create and add our cron file
     echo "*/15 * * * * ${SSH_USER} bash /home/${SSH_USER}/srv/scraper/run_scraper.sh" > scraper_cron
     sudo cp scraper_cron /etc/cron.d/
     rm scraper_cron
+
 STDIN
 
 if [ "$?" -eq "0" ]; then
--- scripts/vps_management/deploy_server.sh
+++ scripts/vps_management/deploy_server.sh
@@ -10,8 +10,10 @@ source ../functions/ssh_and_run.sh
 
 cd ../../
 
+# Remove the _build folder otherwise compilation isn't entirely fresh every time.
 rm -rf server/_build
 
+# Cross-compile build, output it to a local `release` folder. 
 sudo docker build -f deploy.Dockerfile --no-cache --output release .
 sudo chown niko release
 
@@ -20,6 +22,10 @@ RELEASE_FILENAME=$(basename "$RELEASE_FILE")
 
 scp -i "$SSH_KEY" release/release.tar.gz ${SSH_USER}@${SSH_SERVER}:/home/${SSH_USER}/
 
+# Copy over our systemd unit file
+scp -i "$SSH_KEY" scripts/lector.service/lector.service ${SSH_USER}@${SSH_SERVER}:/home/${SSH_USER}
+
+# Write our new Caddyfile with new port
 cat <<-EOF > scripts/caddy.conf/Caddyfile
     lectorchile.com
 
@@ -27,6 +33,13 @@ cat <<-EOF > scripts/caddy.conf/Caddyfile
 EOF
 
 ssh_and_run <<-STDIN
+
+    # Make the required folders for --user space systemd service
+    mkdir -p /home/${SSH_USER}/.config/systemd/user/
+    cp lector.service /home/${SSH_USER}/.config/systemd/user/
+    rm lector.service
+
+    # Untar our release, replace previous files with new ones
     mkdir -p srv/server/ 
     rm -rf srv/server/*
     cp release.tar.gz srv/server/
@@ -34,20 +47,24 @@ ssh_and_run <<-STDIN
     cd srv/server/
     sudo tar -zxvf release.tar.gz
 
-    export LECTOR_PORT=${LECTOR_PORT}
-
+    # Change ownership of new files, SELinux perms for server systemd execution
     sudo chown $SSH_USER /home/${SSH_USER}/srv/server
     sudo chcon -R -t bin_t /home/${SSH_USER}/srv/server/bin/
 
     sudo systemctl reload caddy
 
-    systemctl stop lector
-    systemctl start lector
+    # Start systemd process
+    systemctl --user daemon-reload
+    systemctl --user set-environment LECTOR_PORT=${LECTOR_PORT}
+    systemctl --user stop lector
+    systemctl --user start lector
+
 STDIN
 
-# TODO why does sytemctl stop lector and start lector fail?
+# NOTES:
 
-# The last command above changes the SELinux policy, allowing us to run lector as a service
+# The `chcon` command above changes the SELinux policy, allowing us to run lector as a service
+# We use the --user space for the server explicitly. Env variables must be passed in via set-env or similar to systemd user processes (these don't inherit from .bashrc, etc.).
 
 if [ "$?" -eq "0" ]; then
     echo "Server copied over correctly"
