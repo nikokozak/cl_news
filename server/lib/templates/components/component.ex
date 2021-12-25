55cdf30723526c9029e6110effc95424078a8717
Nikolai Kozak <nikokozak@gmail.com>
2021-12-25 12:42:15 -0600

    pagination for sections working

--- server/lib/templates/components/component.ex
+++ server/lib/templates/components/component.ex
@@ -4,7 +4,7 @@ defmodule Lector.Templates.Component do
   @component_dir 'lib/templates/components'
 
   # Compiled function for pagination
-  EEx.function_from_file(:def, :pagination, Path.join(@component_dir, "pagination.html.eex"), [:items, :count_key, :page])
+  EEx.function_from_file(:def, :pagination, Path.join(@component_dir, "pagination.html.eex"), [:conn, :items, :count_key, :page])
 
   # Compiled function for navigation
   EEx.function_from_file(:def, :navigation, Path.join(@component_dir, "navigation.html.eex"), [])
